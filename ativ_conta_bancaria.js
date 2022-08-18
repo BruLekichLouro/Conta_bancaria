cea80ec14ece2aff99c68c2015145a61ce0c3520
BruLekichLouro <bububio@yahoo.com.br>
2022-08-18 12:19:43 -0300

    Adicionando getter e setter

--- ativ_conta_bancaria.js
+++ ativ_conta_bancaria.js
@@ -4,5 +4,10 @@ class ContaBancaria{
         this.numero = numero;
         this._saldo = saldo; //qdo formos criar get e set, colocamos o _na frente para que as funções tenham nome saldo mas a propriedade não. 
     }
-    
+    get saldo(){
+        return this._saldo;
+    }
+    set saldo(valor){
+        this._saldo=valor;
+    }
 }
